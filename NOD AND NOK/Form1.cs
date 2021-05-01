af4fe4b5d5f483743a2dbb93e2ce451657e0489c
WaVeSsSsS * <nikitakotov88@mail.ru>
2021-05-01 13:32:49 +0300

    Работает теперь)

--- NOD AND NOK/Form1.cs
+++ NOD AND NOK/Form1.cs
@@ -14,21 +14,22 @@ namespace NOD_AND_NOK
     {
         long A, B, R;
         int d = 0;
-        long AB = 1;
+        long AB;
         private void button2_Click(object sender, EventArgs e)
         {  
             long m = Convert.ToInt64(textBox5.Text);
             dataGridView1.RowCount++;
             dataGridView1[0, d].Value = m;
             d++;
-
+          
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Descending);
             A = Math.Abs(Convert.ToInt64(dataGridView1[0, 0].Value));
-            AB = A * AB;
-            for (int i = 1; i < d-1; i++)
+            AB = A;
+            for (int i = 1; i < d - 1; i++)
             {
                 B = Math.Abs(Convert.ToInt64(dataGridView1[0, i + 1].Value));
                 AB = AB * B;
@@ -40,10 +41,8 @@ namespace NOD_AND_NOK
                 }
                 AB = AB / A;
             }
-            AB = AB * A;
-
             textBox3.Text = A.ToString();
-            textBox4.Text = (AB / A).ToString();
+            textBox4.Text = (AB).ToString();
         }
 
         public NOD_AND_NOK()
